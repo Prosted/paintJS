5663799125e75e5f9653390b1267e63df12ae05d
Prosted <69509019+Prosted@users.noreply.github.com>
2020-08-26 14:53:23 +0900

    complete

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColors");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveButton = document.getElementById("jsSave");
+const CANVAS_SIZE = 550;
+const DEFAULT_COLOR = "#2c2c2c";
 
-canvas.width = 550;
-canvas.height = 550;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.strokeStyle = DEFAULT_COLOR;
+ctx.fillStyle = DEFAULT_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -36,6 +42,7 @@ function stopPainting(event) {
 function handleChangeClick(evnet) {
   const color = event.target.style.backgroundColor;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleChangeRange(event) {
@@ -53,11 +60,31 @@ function handleChangeButton(event) {
   }
 }
 
+function handleCanvasClick(event) {
+  if (!filling) {
+    ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+  }
+}
+
+function handleCM(evnet) {
+  event.preventDefault();
+}
+
+function handleSaveButton(evnet) {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "PaintJS[🐱‍👤]";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach((color) =>
@@ -70,4 +97,8 @@ if (range) {
 
 if (mode) {
   mode.addEventListener("click", handleChangeButton);
+}
+
+if (saveButton) {
+  saveButton.addEventListener("click", handleSaveButton);
 }
